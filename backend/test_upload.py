22af999a4169749bb88af10275c298fa5d8878a3
Alpha <alfakiddrock7@gmail.com>
2026-06-18 13:52:18 +0800

    chore: clean up temporary/junk files from repository

--- backend/test_upload.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:8000/documents/upload"
-data = {"workspace_id": "test", "ocr": "false"}
-files = {"file": ("test.txt", b"Hello World")}
-response = requests.post(url, data=data, files=files)
-print(response.status_code)
-print(response.text)
